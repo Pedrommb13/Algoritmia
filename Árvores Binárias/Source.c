8f7aca084521233e0b885ed19bd526bc19b02191
Pedro Barroso <al71134@alunos.utad.pt>
2023-05-11 12:27:23 +0100

    save ig

--- Árvores Binárias/Source.c
+++ Árvores Binárias/Source.c
@@ -1,4 +1,5 @@
 //utilizar os #include necessários
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,9 +11,17 @@
 #define RIGHT(node) ((node)->Right)
 #define EMPTY NULL
 #define NO_LINK NULL
+#define MAX_NAME 20
+#define STAGES 15
 //-------------------------------------------
 typedef enum _STATUS { ERROR = 0, OK = 1 } STATUS;
 typedef enum _BOOLEAN { FALSE = 0, TRUE = 1 } BOOLEAN;
+typedef enum _BTREE_LOCATION { BTREE_LEFT, BTREE_RIGHT } BTREE_LOCATION;	
+typedef struct _PLAYER
+{
+	char name[MAX_NAME];
+	int sets;
+}PLAYER;
 typedef struct _BTREE_NODE {
 	void* Data;
 	struct _BTREE_NODE* Left;
@@ -30,29 +39,47 @@ void PostOrder(BTREE);
 void PreOrder(BTREE);
 STATUS insertIntBST(BTREE*, void*);
 STATUS insertBT(BTREE*, void*, void*, void*);
+STATUS ReadPlayersFromFile(void** players, char* file_name);
+BTREE_NODE *CreateBtree(void** v, int i , int s);
+int BTHeight(BTREE);
+int BTSize(BTREE);
+int BTCountLeaves(BTREE);
+int BTCountNodes(BTREE);
+int PrintLeaves(BTREE);
+void Printwinnergames(BTREE);
 //-------------------------------------------
 int main()
 {
-	srand(time(NULL));
-	BTREE BT;
-	int i;
-	int* pInt;
-	initBTree(&BT);
-	for (i = 0; i < 30; i++)
-	{
-		pInt = (int*)malloc(sizeof(int));
-		*pInt = rand()%100;
-		//*pInt = i;
-		insertIntBST(&BT, pInt);
-	}
+	//srand(time(NULL));
+	//BTREE BT;
+	//int i;
+	//int* pInt;
+	//initBTree(&BT);
+	//for (i = 0; i < 30; i++)
+	//{
+	//	pInt = (int*)malloc(sizeof(int));
+	//	*pInt = rand()%100;
+	//	//*pInt = i;
+	//	insertIntBST(&BT, pInt);
+	//}
+	//InOrder(BT);
+	//printf("\n");
+	//PostOrder(BT);
+	//printf("\n");
+	//PreOrder(BT);
+	//printf("\n");
+	//printf("Altura: %d\n", BTHeight(BT));
+	//printf("Tamanho: %d\n", BTSize(BT));
+	//return 0;
+	BTREE BT = NULL;
+	PLAYER* players[STAGES];
+	ReadPlayersFromFile(players, "torneio.txt");
+	BT = CreateBtree(players, 0, STAGES);
 	InOrder(BT);
-	printf("\n");
-	PostOrder(BT);
-	printf("\n");
-	PreOrder(BT);
-	printf("\n");
-	printf("Altura: %d\n", BTHeight(BT));
-	printf("Tamanho: %d\n", BTSize(BT));
+	printf("\n_________________________________________________________\n");
+	PrintLeaves(BT);
+	printf("\n_________________________________________________________\n");
+	Printwinnergames(BT);
 	return 0;
 }
 /**************************************************************
@@ -85,20 +112,13 @@ BOOLEAN emptyBTree(BTREE BT)
 BOOLEAN isLeaf(BTREE_NODE* pNode)
 {
 	return ((LEFT(pNode) == NULL) && (RIGHT(pNode) == NULL)) ? TRUE : FALSE;
-}
-/**************************************************************
-* Função printBTree(): apresenta no ecrã os elementos da árvore binária
-*
-* Parâmetros: BT – árvore binária
-* Saída: void
-*
-* Esta função não é genérica: só funciona para elementos de tipo INT
-***************************************************************/
+}			
+/**************************************************************/
 void InOrder(BTREE BT)
 {
 	if (emptyBTree(BT) == TRUE) return;
 	InOrder(LEFT(BT));
-	printf("%d, ", *(int*)DATA(BT));
+	printf("\n%s,%d ", ((PLAYER*)DATA(BT))->name, ((PLAYER*)DATA(BT))->sets);
 	InOrder(RIGHT(BT));
 	return;
 }
@@ -134,6 +154,20 @@ int BTSize(BTREE BT)
 {
 	if (emptyBTree(BT) == TRUE) return 0;
 	return BTSize(LEFT(BT)) + BTSize(RIGHT(BT)) + 1;
+}
+
+int BTCountLeaves(BTREE BT)
+{
+	if (emptyBTree(BT) == TRUE) return 0;
+	if (isLeaf(BT) == TRUE) return 1;
+	return BTCountLeaves(LEFT(BT)) + BTCountLeaves(RIGHT(BT));
+}
+
+int BTCountNodes(BTREE BT)
+{
+	if (emptyBTree(BT) == TRUE) return 0;
+	if (isLeaf(BT) == TRUE) return 0;
+	return BTCountNodes(LEFT(BT)) + BTCountNodes(RIGHT(BT)) + 1;
 }
 /**************************************************************
 * Função insertIntBST(): insere um elemento (nó) numa BST
@@ -175,3 +209,105 @@ STATUS createNewBTNode(BTREE_NODE** pNew, void* pData)
 	RIGHT(pTemp) = NULL;
 	return OK;
 }
+/**************************************************************/
+STATUS ReadPlayersFromFile(void** players, char* file_name)
+{
+	FILE* fp;
+	int j, i = 0;
+	void* ptr_data;
+	if ((fp = fopen(file_name, "r")) != NULL)
+	{
+		while (!feof(fp))
+		{
+			if ((ptr_data = malloc(sizeof(PLAYER))) != NULL)
+			{
+				fscanf(fp, "%[^;];", ((PLAYER*)ptr_data)->name);
+				fscanf(fp, "%d\n", &(((PLAYER*)ptr_data)->sets));
+				players[i] = ptr_data;
+				i++;
+			}
+			else
+			{
+				for (j = i; j >= 0; j--)
+					free(players[j]);
+				return(ERROR);
+			}
+		}
+		fclose(fp);
+		return(OK);
+	}
+	else
+		return(ERROR);
+}
+/**************************************************************/
+BTREE_NODE* NewBtreeNode(void* data)
+{
+	BTREE_NODE* tmp_pt;
+	if ((tmp_pt = (BTREE_NODE*)malloc(sizeof(BTREE_NODE))) != NULL)
+	{
+		DATA(tmp_pt) = data;
+		LEFT(tmp_pt) = RIGHT(tmp_pt) = NULL;
+	}
+	return tmp_pt;
+}
+BTREE_NODE* AddBtreeNode(BTREE_NODE* upnode, BTREE_NODE* node, BTREE_LOCATION where)
+{
+	BTREE_NODE* tmp_pt = upnode;
+	if (where == BTREE_LEFT)
+	{
+		if (LEFT(upnode) == NULL)
+			LEFT(upnode) = node;
+		else
+			tmp_pt = NULL;
+	}
+	else
+	{
+		if (RIGHT(upnode) == NULL)
+			RIGHT(upnode) = node;
+		else
+			tmp_pt = NULL;
+	}
+	return tmp_pt;
+}
+BTREE_NODE* InitNode(void* ptr_data, BTREE_NODE* node1, BTREE_NODE* node2)
+{
+	BTREE_NODE* tmp_pt = NULL;
+	tmp_pt = NewBtreeNode(ptr_data);
+	LEFT(tmp_pt) = node1;
+	RIGHT(tmp_pt) = node2;
+	return(tmp_pt);
+}
+BTREE_NODE* CreateBtree(void** v, int i, int size)
+{
+	if (i >= size)
+		return(NULL);
+	else
+		return(InitNode(*(v + i), CreateBtree(v, 2 * i + 1, size), CreateBtree(v, 2 * i + 2, size)));
+}
+/***************************************************************/
+int PrintLeaves(BTREE BT) {
+	if (emptyBTree(BT) == TRUE) return 0;
+	if (isLeaf(BT) == TRUE)
+	{
+		printf("\n%s,%d ", ((PLAYER*)DATA(BT))->name, ((PLAYER*)DATA(BT))->sets);
+		return 1;
+	}
+	return PrintLeaves(LEFT(BT)) + PrintLeaves(RIGHT(BT));
+}
+/**************************************************************/
+int PrintNonLeaves(BTREE BT) {
+	if (emptyBTree(BT) == TRUE) return 0;
+	if (isLeaf(BT) == TRUE) return 0;
+	printf("\n%s,%d ", ((PLAYER*)DATA(BT))->name, ((PLAYER*)DATA(BT))->sets);
+	return PrintNonLeaves(LEFT(BT)) + PrintNonLeaves(RIGHT(BT)) + 1;
+}
+/**************************************************************/
+void Printwinnergames(BTREE BT) {
+	if (isLeaf(BT) == TRUE) return 0;
+	printf("Jogo:\nPlayer: %s->%d vs player: %s->%d\n", ((PLAYER*)DATA(BT->Left))->name, ((PLAYER*)DATA(BT->Left))->sets, ((PLAYER*)DATA(BT->Right))->name, ((PLAYER*)DATA(BT->Right))->sets);
+	printf("Vencerdor: %s", ((PLAYER*)DATA(BT))->name);		
+	printf("\n_________________________________________________________\n");
+	Printwinnergames(LEFT(BT));
+	Printwinnergames(RIGHT(BT));
+}
+/**************************************************************/
